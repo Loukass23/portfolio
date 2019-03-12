0ce04fb6f7c9bd20782768c7f6ff9bb954505d86
Lucas D <l.dupias@net.estia.fr>
2019-03-12 16:31:48 +0100

    added city project

--- src/layout/Projects.js
+++ src/layout/Projects.js
@@ -3,6 +3,7 @@ import Bookstore from '../img/bookstore.jpg';
 import TGIF from '../img/TGIF.jpg';
 import JavaVideo from '../img/battleship.mp4'
 import FootVideo from '../img/football app.mp4'
+import CityVideo from '../img/cityapp.mp4'
 import { Divider } from '@material-ui/core';
 
 
@@ -14,6 +15,33 @@ function Projects() {
                     <div className="card" >
                         <div className="card-body">
                             <p className="card-text"></p>
+                            <div className="row align-items-center no-gutters mt-2 mb-4 mb-lg-5">
+                                <div className="col-xl-8 col-lg-7">
+                                    <video className="video-fluid pr-2" width="30%" height="20%" controls autoPlay={true} loop muted>
+                                        <source src={CityVideo} type="video/mp4" />
+                                    </video>
+                                </div>
+                                <div className="col-xl-4 col-lg-5">
+                                    <div className="featured-text text-center text-lg-left">
+                                        <h4>City-Itinerary Web App</h4>
+                                        <p className="text-black-50 mb-0">Work in Progress: Mobile web app for community itineraries sharing</p>
+                                        <p>Built with Firebase(server functions, database, storage), Javascript, React.js/Redux , HTML, CSS</p>
+
+
+                                        <div className="row d-flex justify-content-center">
+                                            <i className="fab fa-3x fa-reactjs"></i>
+                                            <i className="fab fa-3x fa-css3"></i>
+                                            <i className="fab fa-3x fa-js"></i>
+                                            <i className="fab fa-3x fa-html5"></i>
+                                        </div>
+                                        <a href="https://city-itinerary.firebaseapp.com/">Link to site</a>
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <hr />
                             <div className="row align-items-center no-gutters mb-4 mb-lg-5">
                                 <div className="col-xl-8 col-lg-7">
                                     <img className="img-fluid mb-3 mb-lg-0" src={Bookstore} alt="">
